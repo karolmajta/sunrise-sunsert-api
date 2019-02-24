14247a4377d4d8892e50ec12e2a71865dbf779ad
Karol Majta <karol.majta@L01008008.local>
2019-02-24 13:45:09 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var moment = require('moment');
+
+module.exports = function getSunsriseSunsetData(params) {
+  var lat = params.lat || 0;
+  var lng = params.lng || 0;
+  var date = params.date || moment().format('YYYY-MM-DD');
+  var formatted = params.formatted || 0;
+
+  var url = 'https://api.sunrise-sunset.org/json?lat=' + lat + '&lng=' + lng + '&date=' + date + '&formatted=' + formatted;
+console.log(url);
+
+  return fetch(url)
+    .then(function(r) { return r.json(); });
+};
